704f45050053c6ca548d3287225cf9ade526c170
nono <nonoytnova@gmail.com>
2026-01-11 09:36:35 +0530

    Add Footer component with links and copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,43 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-top">Back to top</div>
 
+      <div className="footer-links">
+        <div>
+          <h4>Get to Know Us</h4>
+          <p>About Amazon</p>
+          <p>Careers</p>
+          <p>Press Releases</p>
+        </div>
+
+        <div>
+          <h4>Connect with Us</h4>
+          <p>Facebook</p>
+          <p>Twitter</p>
+          <p>Instagram</p>
+        </div>
+
+        <div>
+          <h4>Make Money with Us</h4>
+          <p>Sell on Amazon</p>
+          <p>Affiliate Program</p>
+          <p>Advertise Your Products</p>
+        </div>
+
+        <div>
+          <h4>Let Us Help You</h4>
+          <p>Your Account</p>
+          <p>Returns Centre</p>
+          <p>Help</p>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        © 2025 Amazon Clone by You
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
