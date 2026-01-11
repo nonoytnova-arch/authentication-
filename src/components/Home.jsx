e47cffca6dde7d35b1452a152436daa2ffcfd2bd
nono <nonoytnova@gmail.com>
2026-01-11 09:34:59 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,1 +1,17 @@
+import { useNavigate } from "react-router-dom";
 
+function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="home">
+      <h2>Welcome to Amazon Clone</h2>
+
+      <button className="amazon-btn" onClick={() => navigate("/signin")}>
+        Sign in with Amazon
+      </button>
+    </div>
+  );
+}
+
+export default Home;
