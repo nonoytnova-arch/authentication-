4872b4d7e504a61409b04627dcb5289875802812
nono <nonoytnova@gmail.com>
2026-01-11 10:01:42 +0530

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,17 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// Your web app's Firebase configuration
+// 🔴 REPLACE with YOUR Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyC0qzmlC4pzFVHi5KcuFgYR2DdXgIGqDkw",
-  authDomain: "fir-app-2a0ac.firebaseapp.com",
-  projectId: "fir-app-2a0ac",
-  storageBucket: "fir-app-2a0ac.firebasestorage.app",
-  messagingSenderId: "693245701470",
-  appId: "1:693245701470:web:2a766c06e372f929afb3cf"
+  apiKey: "AIzaSyCbmMrJyRv7suPJPj13RuOqyNv5maPaDJA",
+  authDomain: "fir-e-377f4.firebaseapp.com",
+  databaseURL: "https://fir-e-377f4-default-rtdb.firebaseio.com",
+  projectId: "fir-e-377f4",
+  storageBucket: "fir-e-377f4.firebasestorage.app",
+  messagingSenderId: "561175964568",
+  appId: "1:561175964568:web:8779c19004c00e9f7099ed",
+  measurementId: "G-EZYDCNNFTL"
 };
 
 const app = initializeApp(firebaseConfig);
 
+// Firebase Authentication
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
